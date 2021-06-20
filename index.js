4ef8311af487ed373615d6fde73b799b03164d2f
rabeeajahangir <69980221+rabeeajahangir@users.noreply.github.com>
2021-06-19 23:36:49 -0400

    added writefile

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // TODO: Include packages needed for this application
 
-
-const fs = require('fs')
-const inquirer = require('inquirer')
-
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 
 
@@ -13,7 +12,7 @@ const inquirer = require('inquirer')
 // // TODO: Create an array of questions for user input
 // const questions = [];
 
-const userQuestions = () => {
+const promptUse = () => {
   return inquirer.prompt([{
           name: 'git_username',
           type: 'input',
@@ -149,13 +148,26 @@ const userQuestions = () => {
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
-
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 // // TODO: Create a function to initialize app
 // function init() {}
 
 // // Function call to initialize app
 // init();
-profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-  printProfileData(profileDataArgs);
